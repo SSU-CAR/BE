5f512fa7f85d770e44790fbd369984943a74e835
Seohyun-Back <lucymail100@naver.com>
2024-02-16 19:16:29 +0900

    comment: driving package -> 빌드 오류 없음

--- src/main/java/ssucar/driving/controller/DrivingController.java
+++ src/main/java/ssucar/driving/controller/DrivingController.java
@@ -1,25 +1,25 @@
-package ssucar.driving.controller;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ssucar.driving.dto.DrivingDto;
-import ssucar.driving.service.DrivingService;
-import ssucar.dto.SingleResponseDto;
-
-@RestController
-@RequestMapping("/driving")
-public class DrivingController {
-
-    private DrivingService drivingService;
-
-
-    @PostMapping("/start")
-    public ResponseEntity<?> postStartDriving(){
-        return ResponseEntity.ok(drivingService.startDriving());
-    }
-
-}
+//package ssucar.driving.controller;
+//
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import ssucar.driving.dto.DrivingDto;
+//import ssucar.driving.service.DrivingService;
+//import ssucar.dto.SingleResponseDto;
+//
+//@RestController
+//@RequestMapping("/driving")
+//public class DrivingController {
+//
+//    private DrivingService drivingService;
+//
+//
+//    @PostMapping("/start")
+//    public ResponseEntity<?> postStartDriving(){
+//        return ResponseEntity.ok(drivingService.startDriving());
+//    }
+//
+//}
