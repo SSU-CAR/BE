f24ceca07a7f7728b7ce44a0ae374db668c0a82b
Seohyun-Back <lucymail100@naver.com>
2024-02-19 01:41:37 +0900

    ddl-auto: create

--- src/main/resources/data_sql.sql
+++ src/main/resources/data_sql.sql
@@ -1,3 +1,3 @@
-INSERT INTO scenario (name, weight, total) VALUES (1, '차선 변경', 7, 0);
-INSERT INTO scenario (name, weight, total) VALUES (2, '전방 주시 태만', 5, 0);
-INSERT INTO scenario (name, weight, total) VALUES (3, '졸음 운전', 15, 0);
+INSERT INTO scenario (name, weight, total) VALUES ('차선 변경', 7, 0);
+INSERT INTO scenario (name, weight, total) VALUES ('전방 주시 태만', 5, 0);
+INSERT INTO scenario (name, weight, total) VALUES ('졸음 운전', 15, 0);
