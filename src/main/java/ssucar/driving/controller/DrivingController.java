0bed139121c573d76fef2d9b9abb2ebe9d34b126
0woogie <aod8948@naver.com>
2024-02-18 21:58:44 +0900

    DrivingController embedded 관련 코드 추가

--- src/main/java/ssucar/driving/controller/DrivingController.java
+++ src/main/java/ssucar/driving/controller/DrivingController.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ssucar.driving.dto.DrivingDto;
 import ssucar.driving.service.DrivingService;
 import ssucar.dto.SingleResponseDto;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/driving")
@@ -18,6 +21,16 @@ public class DrivingController {
     @Autowired
     private DrivingService drivingService;
 
+    @PostMapping("/embedded")
+    public ResponseEntity embedded(@RequestBody HashMap<String, Object> requestJsonHashMap) {
+
+        int type = (int) requestJsonHashMap.get("type");
+        String createdAt = (String) requestJsonHashMap.get("createdAt");
+        System.out.println("type: " + type);
+        System.out.println("createdAt: " + createdAt);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
     @PostMapping("/start")
     public ResponseEntity<?> postStartDriving(){
