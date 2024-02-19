9fb387798672efbb955c6e613d892dfb2d6264ba
Seohyun-Back <lucymail100@naver.com>
2024-02-20 03:42:58 +0900

    summary: scenarioName 추가

--- src/main/java/ssucar/driving/dto/SummaryDto.java
+++ src/main/java/ssucar/driving/dto/SummaryDto.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SummaryDto {
-    //    @Positive
     private String scenarioName;
     private int scenarioCount;
 }
